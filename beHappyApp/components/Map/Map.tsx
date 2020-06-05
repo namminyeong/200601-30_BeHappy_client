d9f0afb9770b6dc51e11404825af9a96dd1a6036
keuni <kyungeun.kiim@gmail.com>
2020-06-05 23:59:33 +0900

    ADD bring my location
     - 위치 사용 가능 여부 공지
       이후 state로 경도,위도 세팅

--- beHappyApp/components/Map/Map.tsx
+++ beHappyApp/components/Map/Map.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 
 class Map extends React.Component {
+  state = {
+    latitude: 37.52,
+    longitude: 126.97,
+  };
+
+  componentDidMount() {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status === 'granted') {
+        let location = await Location.getCurrentPositionAsync({});
+        console.log(location);
+        let latitude = location.coords.latitude;
+        let longitude = location.coords.longitude;
+        console.log('before', this.state);
+        this.setState({ latitude, longitude });
+        console.log('after', this.state);
+      }
+    })();
+  }
+
   render() {
+    const { latitude, longitude } = this.state;
     return (
       <View style={{ width: '100%', height: '100%' }}>
         <Text>map !!</Text>
@@ -12,8 +34,8 @@ class Map extends React.Component {
           showsUserLocation={false}
           zoomEnabled={true}
           initialRegion={{
-            latitude: 37.52,
-            longitude: 126.97,
+            latitude: latitude,
+            longitude: longitude,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
