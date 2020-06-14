38fef60291678f89039fa4503254773187fa71a0
keuni <kyungeun.kiim@gmail.com>
2020-06-14 18:27:29 +0900

    FIX class to function

--- beHappyApp/components/Map/details/Details.tsx
+++ beHappyApp/components/Map/details/Details.tsx
@@ -3,87 +3,66 @@ import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as Linking from 'expo-linking';
 
-class Details extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // this.handleBookmark = this.handleBookmark.bind(this);
-    this.showDetailHome = this.showDetailHome.bind(this);
-    this.call = this.call.bind(this);
-    this.postDeletebookmark = this.postDeletebookmark.bind(this);
-  }
-
-  // handleBookmark() {
-  //   this.setState({
-  //     bookmark: !this.state.bookmark,
-  //   });
-  // }
-
-  showDetailHome() {
-    this.props.navigation.navigate('DetailsHome', {
-      theCenterInfo: this.props.centerInfo,
+function Details({ navigation, centerInfo, bookmark, postBookmark }) {
+  const showDetailHome = () => {
+    navigation.navigate('DetailsHome', {
+      theCenterInfo: centerInfo,
     });
-  }
+  };
 
-  call() {
-    Linking.openURL(`tel:${this.props.centerInfo.phone}`);
-  }
+  const call = () => {
+    Linking.openURL(`tel:${centerInfo.phone}`);
+  };
 
-  postDeletebookmark() {
-    const { bookmark, postBookmark, centerInfo } = this.props;
+  const postDeletebookmark = () => {
     if (bookmark[centerInfo.id]) {
       postBookmark('DELETE', centerInfo.id);
     } else {
       postBookmark('POST', centerInfo.id);
     }
-    // this.handleBookmark();
-  }
+  };
 
-  render() {
-    const { centerInfo, bookmark } = this.props;
-
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity activeOpacity={1} onPress={this.showDetailHome}>
-          <Text style={styles.text}>{centerInfo.centerName}</Text>
-          <Text style={styles.text}>
-            {centerInfo.roadAddressName}
-            {centerInfo.distance ? (
-              <Text style={styles.distance}>
-                {'  '}
-                {`(`}
-                {centerInfo.distance.toString()}m{`)`}
-              </Text>
-            ) : (
-              ''
-            )}
-          </Text>
-        </TouchableOpacity>
-        {/* <Text style={styles.text}>
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity activeOpacity={1} onPress={showDetailHome}>
+        <Text style={styles.text}>{centerInfo.centerName}</Text>
+        <Text style={styles.text}>
+          {centerInfo.roadAddressName}
+          {centerInfo.distance ? (
+            <Text style={styles.distance}>
+              {'  '}
+              {`(`}
+              {centerInfo.distance.toString()}m{`)`}
+            </Text>
+          ) : (
+            ''
+          )}
+        </Text>
+      </TouchableOpacity>
+      {/* <Text style={styles.text}>
         {centerInfo.phone}
       </Text> */}
-        <View style={{ flexDirection: 'row' }}>
-          <MaterialCommunityIcons
-            name='bookmark'
-            color={bookmark[centerInfo.id] ? 'black' : 'lightgrey'}
-            size={50}
-            style={{ left: 20 }}
-            onPress={this.postDeletebookmark}
-          />
-          <MaterialCommunityIcons
-            name='phone'
-            color='black'
-            size={50}
-            style={{ left: 30 }}
-            onPress={this.call}
-          />
-          <Text style={styles.review}>
-            평점 <Text>{centerInfo.rateAvg}</Text>
-          </Text>
-        </View>
+      <View style={{ flexDirection: 'row' }}>
+        <MaterialCommunityIcons
+          name='bookmark'
+          color={bookmark[centerInfo.id] ? 'black' : 'lightgrey'}
+          size={50}
+          style={{ left: 20 }}
+          onPress={postDeletebookmark}
+        />
+        <MaterialCommunityIcons
+          name='phone'
+          color='black'
+          size={50}
+          style={{ left: 30 }}
+          onPress={call}
+        />
+        <Text style={styles.review}>
+          평점 <Text>{centerInfo.rateAvg}</Text>
+        </Text>
       </View>
-    );
-  }
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
